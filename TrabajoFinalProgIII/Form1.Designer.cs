3146a6bcadb8bc5a1a6e5f256c11abba3889c48c
jefry liz inoa <jefryliz24@gmail.com>
2024-01-04 19:24:05 -0400

    Login

--- TrabajoFinalProgIII/Form1.Designer.cs
+++ TrabajoFinalProgIII/Form1.Designer.cs
@@ -41,6 +41,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -48,10 +50,11 @@
             // Login
             // 
             this.Login.AutoSize = true;
+            this.Login.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.Login.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Login.Location = new System.Drawing.Point(346, 44);
+            this.Login.Location = new System.Drawing.Point(336, 44);
             this.Login.Name = "Login";
-            this.Login.Size = new System.Drawing.Size(92, 36);
+            this.Login.Size = new System.Drawing.Size(73, 30);
             this.Login.TabIndex = 0;
             this.Login.Text = "Login";
             this.Login.Click += new System.EventHandler(this.Login_Click);
@@ -60,45 +63,43 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(193, 120);
+            this.label2.Location = new System.Drawing.Point(221, 89);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 26);
+            this.label2.Size = new System.Drawing.Size(0, 19);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Usuario";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(193, 193);
+            this.label3.Location = new System.Drawing.Point(221, 168);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(119, 26);
+            this.label3.Size = new System.Drawing.Size(0, 19);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Contraseña";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // txtBoxUsuario
             // 
-            this.txtBoxUsuario.Location = new System.Drawing.Point(318, 123);
+            this.txtBoxUsuario.Location = new System.Drawing.Point(226, 129);
             this.txtBoxUsuario.Multiline = true;
             this.txtBoxUsuario.Name = "txtBoxUsuario";
-            this.txtBoxUsuario.Size = new System.Drawing.Size(177, 28);
+            this.txtBoxUsuario.Size = new System.Drawing.Size(249, 28);
             this.txtBoxUsuario.TabIndex = 4;
             // 
             // txtBoxContraseña
             // 
-            this.txtBoxContraseña.Location = new System.Drawing.Point(318, 191);
+            this.txtBoxContraseña.Location = new System.Drawing.Point(226, 216);
             this.txtBoxContraseña.Multiline = true;
             this.txtBoxContraseña.Name = "txtBoxContraseña";
-            this.txtBoxContraseña.Size = new System.Drawing.Size(177, 28);
+            this.txtBoxContraseña.Size = new System.Drawing.Size(249, 28);
             this.txtBoxContraseña.TabIndex = 5;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(4, 44);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 54);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(183, 217);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -109,24 +110,32 @@
             // btnAcceder
             // 
             this.btnAcceder.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnAcceder.Location = new System.Drawing.Point(294, 249);
+            this.btnAcceder.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcceder.Image = ((System.Drawing.Image)(resources.GetObject("btnAcceder.Image")));
+            this.btnAcceder.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAcceder.Location = new System.Drawing.Point(226, 249);
             this.btnAcceder.Margin = new System.Windows.Forms.Padding(2);
             this.btnAcceder.Name = "btnAcceder";
-            this.btnAcceder.Size = new System.Drawing.Size(85, 40);
+            this.btnAcceder.Size = new System.Drawing.Size(109, 40);
             this.btnAcceder.TabIndex = 7;
             this.btnAcceder.Text = "Acceder";
+            this.btnAcceder.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnAcceder.UseVisualStyleBackColor = true;
             this.btnAcceder.Click += new System.EventHandler(this.btnAcceder_Click);
             // 
             // btnLimpiar
             // 
             this.btnLimpiar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnLimpiar.Location = new System.Drawing.Point(392, 249);
+            this.btnLimpiar.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Image = ((System.Drawing.Image)(resources.GetObject("btnLimpiar.Image")));
+            this.btnLimpiar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLimpiar.Location = new System.Drawing.Point(578, 249);
             this.btnLimpiar.Margin = new System.Windows.Forms.Padding(2);
             this.btnLimpiar.Name = "btnLimpiar";
-            this.btnLimpiar.Size = new System.Drawing.Size(91, 40);
+            this.btnLimpiar.Size = new System.Drawing.Size(105, 40);
             this.btnLimpiar.TabIndex = 8;
             this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnLimpiar.UseVisualStyleBackColor = true;
             // 
             // panel1
@@ -137,7 +146,7 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(741, 37);
+            this.panel1.Size = new System.Drawing.Size(698, 37);
             this.panel1.TabIndex = 9;
             // 
             // button1
@@ -163,7 +172,7 @@
             this.label5.Location = new System.Drawing.Point(517, 126);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(211, 25);
+            this.label5.Size = new System.Drawing.Size(176, 20);
             this.label5.TabIndex = 10;
             this.label5.Text = "No estas registrado?";
             // 
@@ -173,7 +182,7 @@
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button2.Location = new System.Drawing.Point(556, 162);
+            this.button2.Location = new System.Drawing.Point(541, 157);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(134, 40);
@@ -181,12 +190,38 @@
             this.button2.Text = "Registrarme";
             this.button2.UseVisualStyleBackColor = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(228, 96);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 21);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Ingrese Su Usuario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(221, 176);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 21);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Ingrese Su Contrasena";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(741, 300);
+            this.ClientSize = new System.Drawing.Size(698, 300);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.panel1);
@@ -225,6 +260,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
