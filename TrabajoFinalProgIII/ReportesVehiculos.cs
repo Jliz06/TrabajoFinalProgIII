e301e68efb1ac9aab06da3bd2f155b081a614af0
Carlos Alberto Reyes Caba <carlosreyescabac140299@gmail.com>
2024-01-09 20:47:07 -0400

    Se agrego el Formulario reporte de vehiculo

---
+++ TrabajoFinalProgIII/ReportesVehiculos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoFinalProgIII
+{
+    public partial class ReportesVehiculos : Form
+    {
+        public ReportesVehiculos()
+        {
+            InitializeComponent();
+        }
+
+        private void btncerrarMenu_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
