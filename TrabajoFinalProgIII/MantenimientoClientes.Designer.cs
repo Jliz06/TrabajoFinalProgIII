ad0dd3fe71e8d9e744f97c2b4576693d02c52361
Carlos Alberto Reyes Caba <carlosreyescabac140299@gmail.com>
2024-01-10 14:56:28 -0400

    Creacion formulario Mantenimiento de clientes

---
+++ TrabajoFinalProgIII/MantenimientoClientes.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace TrabajoFinalProgIII
+{
+    partial class MantenimientoClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCodigoMan = new System.Windows.Forms.TextBox();
+            this.txtNombreMan = new System.Windows.Forms.TextBox();
+            this.txtApellidoMan = new System.Windows.Forms.TextBox();
+            this.txtCorreoMan = new System.Windows.Forms.TextBox();
+            this.txtContactoMan = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnNuevoMan = new System.Windows.Forms.Button();
+            this.btnGuardarMan = new System.Windows.Forms.Button();
+            this.btnEliminarMan = new System.Windows.Forms.Button();
+            this.btnEditarMan = new System.Windows.Forms.Button();
+            this.btnReporteMan = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(480, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(418, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mantenimiento de Clientes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Codigo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(48, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(48, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Apellido:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(48, 363);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Correo:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(48, 428);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Contacto:";
+            // 
+            // txtCodigoMan
+            // 
+            this.txtCodigoMan.Location = new System.Drawing.Point(163, 174);
+            this.txtCodigoMan.Name = "txtCodigoMan";
+            this.txtCodigoMan.Size = new System.Drawing.Size(170, 26);
+            this.txtCodigoMan.TabIndex = 6;
+            // 
+            // txtNombreMan
+            // 
+            this.txtNombreMan.Location = new System.Drawing.Point(163, 239);
+            this.txtNombreMan.Name = "txtNombreMan";
+            this.txtNombreMan.Size = new System.Drawing.Size(170, 26);
+            this.txtNombreMan.TabIndex = 7;
+            // 
+            // txtApellidoMan
+            // 
+            this.txtApellidoMan.Location = new System.Drawing.Point(163, 304);
+            this.txtApellidoMan.Name = "txtApellidoMan";
+            this.txtApellidoMan.Size = new System.Drawing.Size(170, 26);
+            this.txtApellidoMan.TabIndex = 8;
+            // 
+            // txtCorreoMan
+            // 
+            this.txtCorreoMan.Location = new System.Drawing.Point(163, 364);
+            this.txtCorreoMan.Name = "txtCorreoMan";
+            this.txtCorreoMan.Size = new System.Drawing.Size(170, 26);
+            this.txtCorreoMan.TabIndex = 9;
+            // 
+            // txtContactoMan
+            // 
+            this.txtContactoMan.Location = new System.Drawing.Point(163, 429);
+            this.txtContactoMan.Name = "txtContactoMan";
+            this.txtContactoMan.Size = new System.Drawing.Size(170, 26);
+            this.txtContactoMan.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(403, 174);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(640, 281);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // btnNuevoMan
+            // 
+            this.btnNuevoMan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevoMan.Location = new System.Drawing.Point(68, 516);
+            this.btnNuevoMan.Name = "btnNuevoMan";
+            this.btnNuevoMan.Size = new System.Drawing.Size(140, 47);
+            this.btnNuevoMan.TabIndex = 12;
+            this.btnNuevoMan.Text = "Nuevo";
+            this.btnNuevoMan.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardarMan
+            // 
+            this.btnGuardarMan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarMan.Location = new System.Drawing.Point(239, 516);
+            this.btnGuardarMan.Name = "btnGuardarMan";
+            this.btnGuardarMan.Size = new System.Drawing.Size(140, 47);
+            this.btnGuardarMan.TabIndex = 13;
+            this.btnGuardarMan.Text = "Guardar";
+            this.btnGuardarMan.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarMan
+            // 
+            this.btnEliminarMan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarMan.Location = new System.Drawing.Point(403, 516);
+            this.btnEliminarMan.Name = "btnEliminarMan";
+            this.btnEliminarMan.Size = new System.Drawing.Size(140, 47);
+            this.btnEliminarMan.TabIndex = 14;
+            this.btnEliminarMan.Text = "Eliminar";
+            this.btnEliminarMan.UseVisualStyleBackColor = true;
+            // 
+            // btnEditarMan
+            // 
+            this.btnEditarMan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditarMan.Location = new System.Drawing.Point(573, 516);
+            this.btnEditarMan.Name = "btnEditarMan";
+            this.btnEditarMan.Size = new System.Drawing.Size(140, 47);
+            this.btnEditarMan.TabIndex = 15;
+            this.btnEditarMan.Text = "Editar";
+            this.btnEditarMan.UseVisualStyleBackColor = true;
+            // 
+            // btnReporteMan
+            // 
+            this.btnReporteMan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReporteMan.Location = new System.Drawing.Point(903, 516);
+            this.btnReporteMan.Name = "btnReporteMan";
+            this.btnReporteMan.Size = new System.Drawing.Size(140, 47);
+            this.btnReporteMan.TabIndex = 16;
+            this.btnReporteMan.Text = "Ver Reporte";
+            this.btnReporteMan.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TrabajoFinalProgIII.Properties.Resources.Logo_progr;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(237, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MantenimientoClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1093, 616);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnReporteMan);
+            this.Controls.Add(this.btnEditarMan);
+            this.Controls.Add(this.btnEliminarMan);
+            this.Controls.Add(this.btnGuardarMan);
+            this.Controls.Add(this.btnNuevoMan);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtContactoMan);
+            this.Controls.Add(this.txtCorreoMan);
+            this.Controls.Add(this.txtApellidoMan);
+            this.Controls.Add(this.txtNombreMan);
+            this.Controls.Add(this.txtCodigoMan);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MantenimientoClientes";
+            this.Text = "MantenimientoClientes";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCodigoMan;
+        private System.Windows.Forms.TextBox txtNombreMan;
+        private System.Windows.Forms.TextBox txtApellidoMan;
+        private System.Windows.Forms.TextBox txtCorreoMan;
+        private System.Windows.Forms.TextBox txtContactoMan;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnNuevoMan;
+        private System.Windows.Forms.Button btnGuardarMan;
+        private System.Windows.Forms.Button btnEliminarMan;
+        private System.Windows.Forms.Button btnEditarMan;
+        private System.Windows.Forms.Button btnReporteMan;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
